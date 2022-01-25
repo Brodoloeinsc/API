22ba853b592332d5fd27bb7ecac1b9e99b8e94b0
Brodoloeins <joaofernandovascao@gmail.com>
2022-01-24 22:41:43 -0300

    jwt

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -3,14 +3,20 @@ import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import User from '../typeorm/entities/User';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
     email: string;
     password: string;
 }
 
+interface IResponse {
+    user: User;
+    token: string;
+}
+
 export default class CreateSessionService {
-    public async execute({ email, password }: IRequest): Promise<User> {
+    public async execute({ email, password }: IRequest): Promise<IResponse> {
         const userRepository = getCustomRepository(UsersRepository);
         const user = await userRepository.findByEmail(email);
 
@@ -24,6 +30,15 @@ export default class CreateSessionService {
             throw new AppError(`Incorrect Email/Password Combination.`, 401);
         }
 
-        return user;
+        const token = await sign(
+            {},
+            '7ef84f495011a4e024fb1cd0d9e6c6eb69d1bc92',
+            {
+                subject: user.id,
+                expiresIn: '1d'
+            }
+        );
+
+        return { user, token };
     }
 }
